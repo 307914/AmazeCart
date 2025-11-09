0023589a32d0f5eb335c962d9b1783df3e91454a
307914 <abhinay2960@gmail.com>
2025-11-09 11:12:49 +0530

    deploymern ready

--- server3/utils/passwordutils.js
+++ server3/utils/passwordutils.js
@@ -1,17 +1,14 @@
-const {hash,compare,genSalt}=require('bcrypt');
+const { hash, compare, genSalt } = require('bcrypt');
 
-const genPassword=async(password)=>{
-    const salt=await genSalt();
+const genPassword = async (password) => {
+  const salt = await genSalt();
 
-    const passHashed=await hash(password,salt);
+  const passHashed = await hash(password, salt);
 
-    return passHashed;
-}
+  return passHashed;
+};
 
-const verifyPassword=(password,hashpassword)=>{
-    return compare(password,hashpassword);
-
-}
-module.exports={genPassword,verifyPassword};
-
-// genSalt(10).then(ans=>console.log(ans));
+const verifyPassword = (password, hashpassword) => {
+  return compare(password, hashpassword);
+};
+module.exports = { genPassword, verifyPassword };
